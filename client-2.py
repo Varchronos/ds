0e3cb3f3132f806ffd5a8615ea05dad17b9be89d
Varchronos <lcs2020062@iiitl.ac.in>
2022-12-07 03:37:59 +0530

    new updated client and server

--- client-2.py
+++ client-2.py
@@ -1,3 +1,4 @@
+from termcolor import colored
 import socket 
 import threading
 import time 
@@ -7,7 +8,39 @@ PORT = 5053
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MSG = "!DISCONNECT"
-
+connected =  False
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
 
+def chat():
+    connected = True     
+    while connected:
+        msg = input(str("-->"))
+        if(msg == DISCONNECT_MSG):
+            client.send(bytes(DISCONNECT_MSG,FORMAT))
+            connected = False
+        client.send(bytes(msg, FORMAT))
+        # msg =(client.recv(1024).decode(FORMAT))
+        # print(colored("[SERVER]",'green') + f": {msg}")
+        # print(f'\033[1A' + "[SERVER]: {msg}" + '\033[K')
+    
+    if(connected == False):
+        exit()
+
+
+def start():
+    try:
+        client.connect((SERVER, PORT))
+    except:
+        print(colored("SERVER IS OFFLINE!!!", 'magenta') + " Closing client...")
+        exit()
+
+    msg =(client.recv(1024).decode(FORMAT))
+    print(colored("[SERVER]",'green') + f": {msg}")
+    while True:
+        chat()
+        
+    
+        
+
+print(colored("[STARTING]",'red') + "client is starting...")
+start()
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from termcolor import colored
+from datetime import datetime
 import socket 
 import threading
 import time 
@@ -15,25 +17,34 @@ server.bind(ADDR)
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
+    conn.send(bytes("you have joined", FORMAT))
     connected = True
+
     while connected:
-        msg = conn.recv(1024).decode(FORMAT)
-        if msg:
-            if(msg == DISCONNECT_MSG):
-                connected = False
+            msg = conn.recv(1024).decode(FORMAT)
+            time_stamp = time.time()
+            date_time = datetime.fromtimestamp(time_stamp)
+            if msg:
+                if(msg == DISCONNECT_MSG):
+                    connected = False
+                    break
 
-            print(f"[CLIENT {addr}]: {msg}")
+            print(colored(f"[CLIENT {addr}]", 'green') + f": {msg}" + colored(date_time.strftime(" [%H:%M:%S]"),'yellow'))
+            # msg = input(str("-->"))
+            # conn.send(bytes(msg, FORMAT))
 
+
+    print("\n" + colored(f"[CLIENT {addr} has disconnected]", 'red'))
     conn.close()
 
 def start():
     server.listen()
-    print(f"[LISTENING] Server is Listening on {SERVER}")
+    print(colored("[LISTENING]",'red') + f"Server is Listening on {SERVER}")
     while True:
         conn, addr = server.accept()
         thread  = threading.Thread(target = handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        print(colored("[ACTIVE CONNECTIONS]", 'yellow') + f"{threading.active_count() - 1}\n")
 
-print("[STARTING] server is starting...")
+print( colored("[STARTING]",'red') + "server is starting...")
 start()
